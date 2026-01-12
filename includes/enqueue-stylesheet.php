8aad88c5c281c284dcc4d9abd9c0516d52f7801a
Ben Leonard <ben.leonard@creode.co.uk>
2026-01-12 11:14:45 +0000

    Styled login page

--- includes/enqueue-stylesheet.php
+++ includes/enqueue-stylesheet.php
@@ -5,14 +5,35 @@
  * @package Mosaic Admin Style
  */
 
+/**
+ * Version of the mosaic admin style.
+ *
+ * @var string
+ */
+$mosaic_admin_style_version = '3';
+
+/**
+ * Enqueues the mosaic admin style in the admin area.
+ */
 add_action(
 	'admin_enqueue_scripts',
-	function () {
+	function () use ( $mosaic_admin_style_version ) {
 		wp_enqueue_style(
 			'mosaic-admin-style',
 			plugin_dir_url( __FILE__ ) . 'assets/css/style.css',
 			array(),
-			'2'
+			$mosaic_admin_style_version
 		);
+	}
+);
+
+/**
+ * Adds the mosaic admin style to the login page.
+ */
+add_action(
+	'login_head',
+	function () use ( $mosaic_admin_style_version ) {
+		// phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedStylesheet
+		echo '<link rel="stylesheet" type="text/css" href="' . esc_url( plugin_dir_url( __FILE__ ) . 'assets/css/style.css?v=' . $mosaic_admin_style_version ) . '" />';
 	}
 );
